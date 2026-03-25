786d06ee0dfd61f289a95f02bc4d71efa800da7a
Alani Odunlami <83126882+ayomide2021@users.noreply.github.com>
2026-03-25 18:56:18 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="multiple-testing-toolkit",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib"
+    ],
+    author="Your Name",
+    description="A toolkit for multiple hypothesis testing corrections (FDR, Bonferroni)",
+)
